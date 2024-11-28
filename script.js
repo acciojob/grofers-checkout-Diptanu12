88fbe9162fa527408ce9e7531a61d0411a7fb52a
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-11-28 20:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+  const priceElements = document.querySelectorAll(".price");
   
+  let totalPrice = 0;
+  priceElements.forEach(priceElement => {
+    totalPrice += parseFloat(priceElement.textContent);
+  });
+  
+  const table = document.querySelector("table");
+  const totalRow = document.createElement("tr");
+  
+  const totalCell = document.createElement("td");
+  totalCell.setAttribute("colspan", "2");
+  totalCell.textContent = `Total Price: Rs ${totalPrice}`;
+  
+  totalRow.appendChild(totalCell);
+  table.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
